d5234dec48474b6010c72744ec5d485d72da7b97
t.prohoda <tatiana.prohoda@vedidev.com>
2019-04-17 23:43:25 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-
+import './App.css';
+import english_game from './english_game.png';
 class App extends Component {
 render () {
-  return <div>Hello world</div>;
+  return <div>
+    <img 
+        src={english_game} className="english_game"
+    
+      />
+ <button type="button" class="btn margin btn-danger">ГЛАВНАЯ СТРАНИЦА</button>
+ <button type="button" class="btn margin2 btn-danger">ИГРА</button>
+ <button type="button" class="btn margin2 btn-danger">ЛИЧНАЯ КОМНАТА</button>
+ <button type="button" class="btn margin2 btn-danger">ЧАТ</button>
+</div>
   }
 }
 
