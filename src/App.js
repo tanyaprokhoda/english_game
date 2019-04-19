7a878aa43e9dd0a61366f614a6fa6e6a7109e2ea
t.prohoda <tatiana.prohoda@vedidev.com>
2019-04-19 09:41:25 +0300

    Start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
 import './App.css';
-import english_game from './english_game.png';
-class App extends Component {
+const data = [
+      {
+        img: 'pic/apple.png',
+        words:['chicken', 'water', 'apple', 'cheese', 'eggs', 'milk', 'tea'],
+      correct: 2
+      },
+    {
+      img: 'pic/cheese.png',
+      words:['water', 'cheese', 'apple', 'carrot', 'cucumber', 'potato', 'rice'],
+      correct: 1
+      },
+      {
+        img: 'pic/pineapple.png',
+        words:['banana', 'blackberry', 'blueberry', 'cherry', 'lemon', 'orange', 'pineapple'],
+      correct: 6
+      },
+      {
+        img: 'pic/bread.png',
+        words:['bread', 'cabbage', 'candy', 'fish', 'lemon', 'orange', 'meat'],
+      correct: 0
+      },
+    {
+      img: 'pic/orange.png',
+      words:['banana', 'blackberry', 'mushroom', 'cherry', 'lemon', 'orange', 'pineapple'],
+      correct: 5
+      },
+      {
+        img: 'pic/banana.png',
+        words:['banana', 'melon', 'blueberry', 'mushroom', 'lemon', 'orange', 'pineapple'],
+      correct: 0
+      },
+    {
+      img: 'pic/potato.png',
+      words:['banana', 'nut', 'peach', 'pepper', 'lemon', 'potato', 'pineapple'],
+      correct: 5
+      },
+    {
+      img: 'pic/lemon.png',
+      words:['chocolate', 'apricot', 'tea', 'cherry', 'lemon', 'orange', 'pineapple'],
+      correct: 4
+      },
+      {
+        img: 'pic/tomato.png',
+        words:['banana', 'tomato', 'blueberry', 'cherry', 'lemon', 'orange', 'pineapple'],
+      correct: 1
+      },
+    {
+      img: 'pic/pear.png',
+      words:['banana', 'blackberry', 'blueberry', 'pear', 'lemon', 'orange', 'pineapple'],
+      correct: 3
+      },
+      {
+        img: 'pic/meat.png',
+        words:['banana', 'blackberry', 'meat', 'cherry', 'lemon', 'orange', 'pineapple'],
+      correct: 2
+      },
+      {
+        img: 'pic/mushroom.png',
+        words:['banana', 'mushroom', 'blueberry', 'cherry', 'plum', 'pie', 'pineapple'],
+      correct: 1
+      },
+ ]
+ export default class Game extends Component {
 render () {
-  return <div>
-    <img 
-        src={english_game} className="english_game"
-    
-      />
- <button type="button" class="btn margin btn-danger">ГЛАВНАЯ СТРАНИЦА</button>
- <button type="button" class="btn margin2 btn-danger">ИГРА</button>
- <button type="button" class="btn margin2 btn-danger">ЛИЧНАЯ КОМНАТА</button>
- <button type="button" class="btn margin2 btn-danger">ЧАТ</button>
-</div>
-  }
-}
-
-export default App;
+  return (
+     data.map((obj) => <img className="pic" src={obj.img}/>)
+  )
+    }
+ }
