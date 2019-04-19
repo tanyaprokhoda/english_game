06b23509a8254c7878f233b7d397b0174d708613
Ivan Oleynik <John316@i.ua>
2019-04-19 16:32:40 +0300

    added main page

--- src/StartPage.js
+++ src/StartPage.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import english_game from './images/english_game.png';
-class App extends Component {
-render () {
-  return <div>
-            <img src={english_game} className="english_game" />
-            <button type="button" class="btn margin btn-danger">ГЛАВНАЯ СТРАНИЦА</button>
-            <button type="button" class="btn margin2 btn-danger">ИГРА</button>
-            <button type="button" class="btn margin2 btn-danger">ЛИЧНАЯ КОМНАТА</button>
-            <button type="button" class="btn margin2 btn-danger">ЧАТ</button>
-        </div>
-  }
+import App from "./App";
+class StartPage extends Component {
+    getContent () {
+        const currentPage = 'game';
+        if(currentPage === 'game'){
+            return <App/>
+        }
+    }
+    render () {
+            return <div>
+                <img src={english_game} className="english_game" />
+                <button type="button" class="btn margin btn-danger">ГЛАВНАЯ СТРАНИЦА</button>
+                <button type="button" class="btn margin2 btn-danger">ИГРА</button>
+                <button type="button" class="btn margin2 btn-danger">ЛИЧНАЯ КОМНАТА</button>
+                <button type="button" class="btn margin2 btn-danger">ЧАТ</button>
+                <div className='content'>
+                    {this.getContent()}
+                </div>
+            </div>
+    }
 }
 
-export default App;
+export default StartPage;
